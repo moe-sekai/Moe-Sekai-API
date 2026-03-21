d7ddf1dc425f5c3eb2a6075bbc46b95db2f44a0b
luoxiadesu <luoxia7517@Gmail.com>
2026-03-21 09:14:44 +0800

    修改

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,8 @@
 set -eu
 
 DEFAULT_CONFIG="/app/moe-sekai-configs.example.yaml"
-PERSISTENT_CONFIG="/data/moe-sekai-configs.yaml"
 
 mkdir -p /data /data/accounts /data/master /data/versions
-
-if [ -f "$PERSISTENT_CONFIG" ]; then
-  export CONFIG_PATH="$PERSISTENT_CONFIG"
-else
-  export CONFIG_PATH="$DEFAULT_CONFIG"
-fi
+export CONFIG_PATH="$DEFAULT_CONFIG"
 
 exec /app/moe-sekai-api
